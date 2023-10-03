d948dc777311af7fe00d7f345364ede056c4a790
Gold-prpr <33225@ojs.ac.jp>
2023-10-03 10:56:18 +0900

    マップ更新

--- aqua/game/src/game/game_object/stage/stage.cpp
+++ aqua/game/src/game/game_object/stage/stage.cpp
@@ -51,8 +51,8 @@ void CStage::Draw(void)
 
 	for (auto it = m_MapData.begin(); it != m_MapData.end(); ++it, ++i)
 	{
-		m_TileSprite[*it].position.x = (float)(i % map_x) * 60 - m_Scroll.x;
-		m_TileSprite[*it].position.y = (float)(i / map_x) * 60 - m_Scroll.y;
+		m_TileSprite[*it].position.x = (float)(i % map_x) * map_chip_size - m_Scroll.x;
+		m_TileSprite[*it].position.y = (float)(i / map_x) * map_chip_size - m_Scroll.y;
 
 		m_TileSprite[*it].Draw();
 	}
