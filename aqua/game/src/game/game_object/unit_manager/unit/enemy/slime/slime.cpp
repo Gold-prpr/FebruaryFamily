130085a1e2d1de02d3abdf0fa4ada9b2a372c313
apanti <33228@ojs.ac.jp>
2023-11-01 19:06:59 +0900

    更新

---
+++ aqua/game/src/game/game_object/unit_manager/unit/enemy/slime/slime.cpp
@@ -0,0 +1,21 @@
+#include "slime.h"
+
+CSlime::CSlime(aqua::IGameObject* parent)
+{
+}
+
+void CSlime::Initialize(const aqua::CVector2& position)
+{
+}
+
+void CSlime::Update()
+{
+}
+
+void CSlime::Draw()
+{
+}
+
+void CSlime::Finalize()
+{
+}
