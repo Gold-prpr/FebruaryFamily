ac08c51c7ce0530fadb563de16c103abc1c923d4
sirogane33149 <33149@ojs.ac.jp>
2023-11-14 10:03:51 +0900

    更新
    順位の仮位置

---
+++ aqua/game/src/game/game_object/ui_manager/ui_component/rank_icon/rank_icon.h
@@ -0,0 +1,29 @@
+#include "aqua.h"
+#include "../ui_component.h"
+
+class CRankIcon
+	: public IUiComponent
+{
+public:
+	//コンストラクタ
+	CRankIcon(aqua::IGameObject* parent);
+
+	//デストラクタ
+	~CRankIcon(void) = default;
+
+	//初期化
+	void Initialize(const aqua::CVector2& position)override;
+
+	//更新
+	void Update(void) override;
+
+	//描画
+	void Draw(void)override;
+
+	//解放
+	void Finalize(void)override;
+
+private:
+	aqua::CSprite		m_1PRankIconSprite;	//1Pアイテムアイコン
+	aqua::CSprite		m_2PRankIconSprite;	//2Pアイテムアイコン
+};
--- aqua/game/src/game/game_object/ui_manager/ui_manager.cpp
+++ aqua/game/src/game/game_object/ui_manager/ui_manager.cpp
@@ -1,9 +1,11 @@
 #include "ui_manager.h"
 #include "ui_component/item_frame/item_frame.h"
 #include "ui_component/item_icon/item_icon.h"
+#include "ui_component/rank_icon/rank_icon.h"
 
 const aqua::CVector2	CUiManager::m_ui_item_frame_position = aqua::CVector2(0.0f, 0.0f);
 const aqua::CVector2	CUiManager::m_ui_item_icon_position = aqua::CVector2(0.0f, 0.0f);
+const aqua::CVector2	CUiManager::m_ui_rank_icon_position = aqua::CVector2(0.0f, 0.0f);
 
 CUiManager::CUiManager(aqua::IGameObject* parent)
 {
@@ -40,4 +42,8 @@ void CUiManager::Create(void)
 	CItemIcon* itemicon = aqua::CreateGameObject<CItemIcon>(this);
 
 	itemicon->Initialize(m_ui_item_icon_position);
+
+	CRankIcon* rankicon = aqua::CreateGameObject<CRankIcon>(this);
+
+	rankicon->Initialize(m_ui_rank_icon_position);
 }
