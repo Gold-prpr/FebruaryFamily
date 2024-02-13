7eea8479b7d9c571f0ecedd2666eb30786d01c04
sirogane33149 <33149@ojs.ac.jp>
2024-02-13 10:50:25 +0900

    更新
    アイテム追加

---
+++ aqua/game/src/game/game_object/item_manager/item/reverse_item/reverse_item.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../item.h"
+
+class CUnitManager;
+class CPlayer;
+class CEffectIcon;
+
+class CReverseItem
+	:public IItem
+{
+public:
+	//コンストラクタ
+	CReverseItem(aqua::IGameObject* parent);
+
+	//デストラクタ
+	~CReverseItem(void) = default;
+
+	//初期化
+	void Initialize(aqua::controller::DEVICE_ID other_id);
+
+	//更新
+	void Update()override;
+
+	//操作逆
+	void Reverse();
+
+	bool m_ReverseFlag;
+
+private:
+	CUnitManager* m_pUnitManager;
+	CPlayer* m_pPlayer;
+	CEffectIcon* m_pEffectIcon;
+};
